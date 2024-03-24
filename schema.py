36d6bc8867f945147b50ed95c5c529a9041ddf95
Aman Tyagi <63581575+T-Aman19@users.noreply.github.com>
2024-03-24 19:37:25 +0530

    Update schema.py

--- schema.py
+++ schema.py
@@ -8,3 +8,27 @@ class MissionParameters(BaseModel):
     Overlap:int
     Altitude:int
     Speed:int
+    class Config:
+        schema_extra = {
+            "example": {
+                "FOV": 84,
+                "AOI": {
+                    "type": "Polygon",
+                    "coordinates": [
+                        [
+                            [77.395034, 28.49966],
+                            [77.395077, 28.494889],
+                            [77.401257, 28.495247],
+                            [77.401085, 28.498302],
+                            [77.395034, 28.49966]
+                        ]
+                    ]
+                },
+                "ImageHeight": 6000,
+                "ImageWidth": 8000,
+                "Overlap": 80,
+                "Altitude": 100,
+                "Speed": 15
+            }
+        }
+
